822a0f253670dfc0830b67f4cbabc47d7573a39b
durin93 <lsc401@naver.com>
2018-09-12 14:34:10 +0900

    init

---
+++ src/main/java/com/durin93/bookmanagement/BookmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.durin93.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanagementApplication.class, args);
+    }
+}
